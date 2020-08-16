afe020b2725109e42e3b25e3e4dd2ea68da2c14c
schein00 <schein.jefer@gmail.com>
2020-08-16 20:59:43 -0300

    :construction: api creation and adjustments

---
+++ hours_front/src/components/Grid.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Grid(form) {
+  console.log(form);
+
+  return <p>grid 1</p>;
+}
--- hours_front/src/components/MainPage.js
+++ hours_front/src/components/MainPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import '../css/MainPage.css';
 
+import Grid from './Grid';
+
 export default function MainPage() {
   return (
     <div className="principal">
-      <p>teste inicial</p>
+      <Grid form="0" />
     </div>
   );
 }
