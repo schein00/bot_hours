bf9238d70345f2b5528c3c84c73cfd817270f66a
schein00 <schein.jefer@gmail.com>
2020-08-16 03:28:26 -0300

    :construction: initial commit

---
+++ hours_front/src/components/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../css/MainPage.css';
+
+export default function MainPage() {
+  return (
+    <div className="principal">
+      <p>teste inicial</p>
+    </div>
+  );
+}
