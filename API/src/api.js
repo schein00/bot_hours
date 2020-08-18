296dd88c2068ce31feb7d66ea87c6e6cf040f016
schein00 <schein.jefer@gmail.com>
2020-08-17 22:33:13 -0300

    :alien: update rthe main api for return the data from the grid

--- API/src/api.js
+++ API/src/api.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 
+var port = 30300;
+
+const fs = require('fs');
+const file = './data/dados.json';
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
+app.get('/grid/:form', (req, res) => {
+  let grids = JSON.parse(fs.readFileSync(file, 'utf8'));
+  let forms = parseInt(req.params.form);
+
+  let f = grids.forms.find((element) => element.id == forms);
+
+  console.log(grids.forms);
+  console.log(f);
+
+  res.status(200).send(f.formulario);
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
